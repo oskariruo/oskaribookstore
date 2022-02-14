27512504ee9b5cb2a8ea368f3d91d53e427d5b49
Oskari Ruotsalainen <78789083+oskariruo@users.noreply.github.com>
2022-02-14 17:00:12 +0200

    First commit

--- src/main/java/com/example/oskaribookstore/model/Book.java
+++ src/main/java/com/example/oskaribookstore/model/Book.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Book {
@@ -16,16 +18,22 @@ public class Book {
 	private int year;
 	private String isbn;
 	private double price;
-
+	
+	 @ManyToOne
+	    @JoinColumn(name = "categoryid")
+	    private Category category;
+	 
 	public Book() {
+		super();
 	}
 
-	public Book(String title, String author, int year, String isbn, double price) {
+	public Book(String title, String author, int year, String isbn, double price, Category category) {
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -74,6 +82,13 @@ public class Book {
 
 	public void setPrice(double price) {
 		this.price = price;
+	}
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
 	}
 
 	@Override
--- src/main/java/com/example/oskaribookstore/web/BookController.java
+++ src/main/java/com/example/oskaribookstore/web/BookController.java
@@ -1,5 +1,7 @@
 package com.example.oskaribookstore.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.oskaribookstore.model.Book;
 import com.example.oskaribookstore.model.BookRepository;
+import com.example.oskaribookstore.model.CategoryRepository;
 
 @Controller
 public class BookController {
 	@Autowired
 	private BookRepository repository; 
+	
+	@Autowired
+	private CategoryRepository crepository;
 	
     @RequestMapping(value= {"/", "/booklist"})
     public String bookList(Model model) {	
@@ -25,8 +31,9 @@ public class BookController {
     @RequestMapping(value = "/add")
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
+        model.addAttribute("categories", crepository.findAll());
         return "addbook";
-    }     
+    } 
     
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(Book book){
@@ -41,8 +48,10 @@ public class BookController {
     }
     
     @RequestMapping(value = "/editbook/{id}", method = RequestMethod.GET)
-    public String editStudent(@PathVariable("id") Long bookId, Model model){
-    	model.addAttribute("book", repository.findById(bookId));
+    public String modifyBook(@PathVariable("id") Long bookId, Model model) {
+    	Optional<Book> book = repository.findById(bookId);
+        model.addAttribute("categories", crepository.findAll());
+    	model.addAttribute("book", book);
     	return "editbook";
     }     
     @RequestMapping(value = "/process", method = RequestMethod.POST)
